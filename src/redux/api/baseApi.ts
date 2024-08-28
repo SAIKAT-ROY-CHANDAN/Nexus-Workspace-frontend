0d2ab105b65d2e403a15e7c1824c22fd05ee542b
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-08-28 19:49:24 +0600

    redux setup is done

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
+    endpoints: (builder) => ({
+        getRooms: builder.query({
+            query: () => {
+                return {
+                    url: '/rooms'
+                }
+            }
+        })
+    })
+})
+
+export const { useGetRoomsQuery, useLazyGetRoomsQuery } = baseApi
