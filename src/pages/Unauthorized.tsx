edda6268c83e2bbaef625540f06de7ab20de027c
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-09-28 14:14:29 +0600

    updated the unauthorized page

--- src/pages/Unauthorized.tsx
+++ src/pages/Unauthorized.tsx
@@ -1,7 +1,25 @@
+import '@/styles/unauthorized.css'
+
 const Unauthorized = () => {
   return (
-    <div className="text-4xl text-center">
-        You Are not Authorized to visit this route
+    <div className='translate-y-28'>
+      <div className="gandalf">
+        <div className="fireball"></div>
+        <div className="skirt"></div>
+        <div className="sleeves"></div>
+        <div className="shoulders">
+          <div className="hand left"></div>
+          <div className="hand right"></div>
+        </div>
+        <div className="head">
+          <div className="hair"></div>
+          <div className="beard"></div>
+        </div>
+      </div>
+      <div className="message">
+        <h1>403 - You Shall Not Pass</h1>
+        <p>Uh oh, Gandalf is blocking the way!<br />Maybe you have tried to access the route that you'r not meant too</p>
+      </div>
     </div>
   )
 }
